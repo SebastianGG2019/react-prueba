1cb13d7392f1589b5f5aa72cbe2de782fc5f9315
Sebastian <Sebasguz.g12@gmail.com>
2021-06-11 15:06:25 -0500

    Validaciones
    Validaciones

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -24,10 +24,10 @@ const FormInput = () => {
             <h1 className='Bienvenida'>Bienvenido Elige tu aerolinea</h1>
              <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="inputs-form">
-                        <input type="text" name="nombre" placeholder="Nombre completo" required="required" {...register('nombre')}/>
-                        <input type="email" name="email"  placeholder="Email" required="required" {...register('emai')}/>
-                        <input type="text" name="celular"  placeholder="celular" required="required" {...register('celular')}/>
-                        <input type="text" name="rangoEdad"  placeholder="Rango de edad" required="required" {...register('rangoEdad')}/>
+                        <input type="text" name="nombre" placeholder="Nombre completo" required="required"  minLength="10" maxLength="40"  {...register('nombre')}/>
+                        <input type="email" name="email"  placeholder="Email" required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" {...register('emai')}/>
+                        <input type="text" name="celular"  placeholder="celular" required="required" minLength="10" maxLength="10" pattern="[0-9]+" {...register('celular')}/>
+                        <input type="text" name="rangoEdad"  placeholder="Rango de edad" required="required" minLength="10" maxLength="10"{...register('rangoEdad')}/>
                     </div>
                     <button className="btn btn-primary send-form-ct" >Enviar</button>
                 </form>
